9711c90039fd885620fd40ec9a9e07de90d0a3c0
Louis Dionne <ldionne.2@gmail.com>
2017-09-13 06:52:00 -0700

    Add examples for vtable policies

---
+++ code/joined_vtable.dyno.cpp
@@ -0,0 +1,66 @@
+// Copyright Louis Dionne 2017
+// Distributed under the Boost Software License, Version 1.0.
+
+#include "vtable.dyno.hpp"
+
+#include <dyno.hpp>
+
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace dyno::literals;
+
+
+// sample(vehicle)
+struct vehicle {
+  template <typename Any>
+  vehicle(Any v) : poly_{v} { }
+
+  void accelerate() {
+    poly_.virtual_("accelerate"_s)(poly_);
+  }
+
+private:
+  using VTable = dyno::vtable<
+                  dyno::local<dyno::only<decltype("accelerate"_s)>>,
+                  dyno::remote<dyno::everything>>;
+  //             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  dyno::poly<Vehicle, dyno::remote_storage, VTable> poly_;
+};
+// end-sample
+
+
+//////////////////////////////////////////////////////////////////////////////
+struct Car {
+  std::string make;
+  int year;
+  void accelerate() { std::cout << "Car::accelerate()" << std::endl; }
+};
+
+struct Truck {
+  std::string make;
+  int year;
+  void accelerate() { std::cout << "Truck::accelerate()" << std::endl; }
+};
+
+struct Plane {
+  std::string make;
+  std::string model;
+  void accelerate() { std::cout << "Plane::accelerate()" << std::endl; }
+};
+
+// sample(main)
+int main() {
+  std::vector<vehicle> vehicles;
+
+  vehicles.push_back(Car{"Audi", 2017});
+  vehicles.push_back(Truck{"Chevrolet", 2015});
+  vehicles.push_back(Plane{"Boeing", "747"});
+
+  for (auto& vehicle : vehicles) {
+    vehicle.accelerate();
+  }
+}
+// end-sample
