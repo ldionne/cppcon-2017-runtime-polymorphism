538ff36ae7f53629759cbc0463e3ef8159c30ba4
Louis Dionne <ldionne.2@gmail.com>
2017-09-27 20:30:25 -0700

    Last minute improvements

--- code/shared_remote_storage.dyno.cpp
+++ code/shared_remote_storage.dyno.cpp
@@ -1,56 +1,31 @@
 // Copyright Louis Dionne 2017
 // Distributed under the Boost Software License, Version 1.0.
 
-#include "vtable.hpp"
+#include "vtable.dyno.hpp"
+
+#include <dyno.hpp>
 
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace dyno::literals;
 
 
 // sample(Vehicle)
 struct Vehicle {
-  vtable const* const vptr_;
-  union { void* ptr_;
-          std::aligned_storage_t<16> buffer_; };
-  bool on_heap_;
-
   template <typename Any>
-  Vehicle(Any vehicle) : vptr_{&vtable_for<Any>} {
-    if (sizeof(Any) > 16) {
-      on_heap_ = true;
-      ptr_ = new Any(vehicle);
-    } else {
-      on_heap_ = false;
-      new (&buffer_) Any{vehicle};
-    }
-  }
+  Vehicle(Any vehicle) : poly_{vehicle} { }
 
   void accelerate()
-  { vptr_->accelerate(on_heap_ ? ptr_ : &buffer_); }
-// end-sample
+  { poly_.virtual_("accelerate"_s)(poly_); }
 
-  Vehicle(Vehicle const& other) : vptr_{other.vptr_} {
-    if (other.on_heap_) {
-      ptr_ = other.vptr_->clone(other.ptr_);
-    } else {
-      other.vptr_->copy(&buffer_, &other.buffer_);
-    }
-  }
-
-  ~Vehicle() {
-    if (on_heap_) {
-      vptr_->delete_(ptr_);
-    } else {
-      vptr_->dtor(&buffer_);
-    }
-  }
-// sample(Vehicle)
+private:
+  dyno::poly<IVehicle, dyno::shared_remote_storage> poly_;
+  //                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 };
 // end-sample
-
 
 
 //////////////////////////////////////////////////////////////////////////////
